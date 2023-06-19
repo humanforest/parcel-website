bc980df2664f79da1a2a21d39053a9ab398d1918
José Duarte <jduartea@gmail.com>
2023-06-19 19:23:38 -0400

    feat(fonts): add GT Haptik fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,7 +1,39 @@
 import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import localFont from "next/font/local"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
+export const fontSans = localFont({
+  src: [
+    {
+      path: "../assets/fonts/GT-Haptik-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GT-Haptik-Light-Oblique.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/GT-Haptik-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GT-Haptik-Regular-Oblique.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/GT-Haptik-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GT-Haptik-Bold-Oblique.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
   variable: "--font-sans",
 })
 
