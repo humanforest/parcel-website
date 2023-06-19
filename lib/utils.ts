0c0d328beaa9816c60b7bf92081b639a371cb893
José Duarte <jduartea@gmail.com>
2023-06-19 19:29:07 -0400

    refactor(utils): add shadow classes to twmerge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { extendTailwindMerge } from "tailwind-merge"
 
+const customTwMerge = extendTailwindMerge({
+  classGroups: {
+    boxShadow: [
+      { shadow: ["none", "offset-xs", "offset-sm", "offset-md", "offset-lg"] },
+    ],
+  },
+})
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return customTwMerge(clsx(inputs))
 }
