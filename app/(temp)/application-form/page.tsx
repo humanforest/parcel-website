83d23751a0b0dfa3e30a749f265e0b585ef81eb1
José Duarte <jduartea@gmail.com>
2023-06-29 10:01:29 -0400

    feat(couriers): temporary landing page for couriers

---
+++ app/(temp)/application-form/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Widget } from "@typeform/embed-react";
+
+export default function Page() {
+  return (
+    <div>
+      <Widget id="QL1aiZ3A" className="h-screen w-full" />
+    </div>
+  )
+}
